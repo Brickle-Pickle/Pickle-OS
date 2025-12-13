1c38bde9a63a30d6cf5dcf16e17ead44ee6d5ee0
Brickle Pickle <bricklepicklegs@gmail.com>
2025-12-13 12:21:35 +0100

    Folder structure

---
+++ mkfs.py
@@ -0,0 +1,20 @@
+# Filesystem creation script (mkfs.py, only the first time)
+import os
+
+folders = [
+    "system",
+    "apps",
+    "bin",
+    "home",
+    "home/user",
+    "data",
+    "data/logs"
+]
+
+for f in folders:
+    try:
+        os.mkdir(f)
+    except OSError:
+        pass
+
+print("Filesystem structure created.")
